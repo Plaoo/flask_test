ebf4e142fb1cf1d254c739bd191df459fbeb41e8
Plaoo <paolo.monni345@gmail.com>
2017-06-13 19:59:53 +0200

    sistemata la dichiarazione della dir dei file html

--- esempi/html_esterno.py
+++ esempi/html_esterno.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask import render_template
 import os
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='../templates')
+#meglio dichiarare dove si trova la dir con i templates
 
 @app.route("/")
 def index():
     autore = "Orwell"
 
-    return render_template('./templates/index_autore.html', autore = autore)
+    return render_template('index_autore.html', autore = autore)
